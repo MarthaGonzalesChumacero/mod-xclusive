58fc903880d70cb87e42c2a5c3292af59b709f75
Martha Gonzales Chumacero <marthagonzales.sis24ch@tecba.edu.bo>
2026-09-16 19:35:55 -0400

    Corregir rutas para GitHub Pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,32 +16,51 @@ function App() {
   return (
     <AuthProvider>
       <CarritoProvider>
-        <BrowserRouter>
+        <BrowserRouter basename="/mod-xclusive">
           <Routes>
             {/* Rutas sin Navbar (auth) */}
             <Route path="/login" element={<Login />} />
             <Route path="/registro" element={<Registro />} />
 
             {/* Rutas con Navbar */}
-            <Route path="/*" element={
-              <div className="min-h-screen flex flex-col bg-neutral-100">
-                <Navbar />
-                <main className="flex-grow">
-                  <Routes>
-                    <Route path="/" element={<Inicio />} />
-                    <Route path="/productos" element={<Productos />} />
-                    <Route path="/productos/:id" element={<DetalleProducto />} />
-                    <Route path="/carrito" element={<Carrito />} />
-                    <Route path="/perfil" element={
-                      <RutaProtegida><Perfil /></RutaProtegida>
-                    } />
-                    <Route path="/admin" element={
-                      <RutaProtegida soloAdmin><Admin /></RutaProtegida>
-                    } />
-                  </Routes>
-                </main>
-              </div>
-            } />
+            <Route
+              path="/*"
+              element={
+                <div className="min-h-screen flex flex-col bg-neutral-100">
+                  <Navbar />
+
+                  <main className="flex-grow">
+                    <Routes>
+                      <Route path="/" element={<Inicio />} />
+                      <Route path="/productos" element={<Productos />} />
+                      <Route
+                        path="/productos/:id"
+                        element={<DetalleProducto />}
+                      />
+                      <Route path="/carrito" element={<Carrito />} />
+
+                      <Route
+                        path="/perfil"
+                        element={
+                          <RutaProtegida>
+                            <Perfil />
+                          </RutaProtegida>
+                        }
+                      />
+
+                      <Route
+                        path="/admin"
+                        element={
+                          <RutaProtegida soloAdmin>
+                            <Admin />
+                          </RutaProtegida>
+                        }
+                      />
+                    </Routes>
+                  </main>
+                </div>
+              }
+            />
           </Routes>
         </BrowserRouter>
       </CarritoProvider>
